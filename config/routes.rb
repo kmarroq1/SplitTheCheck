c0d586811f24814c83432c430f2a57e2409aed82
kmarroq1 <kmarroq1@cs6242-vm09.uwg.westga.edu>
2022-04-10 23:49:29 -0400

    Added user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :restaurants, except: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'restaurants#index'
   put '/restaurant/:id/splitVote', to: 'restaurants#splitVote', as: 'splitVote'
   put '/restaurant/:id/noSplitVote', to: 'restaurants#voteNoSplit', as: 'voteNoSplit'
   get 'search', to: 'restaurants#search'

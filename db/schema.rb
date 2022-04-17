e35829bd5b6c4617a3782d1bb738dbe529936fd5
kmarroq1 <kmarroq1@cs6242-vm09.uwg.westga.edu>
2022-04-17 11:35:09 -0400

    Reset votes table creation to include correct column references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_11_014826) do
+ActiveRecord::Schema.define(version: 2022_04_17_152803) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -33,4 +33,16 @@ ActiveRecord::Schema.define(version: 2022_04_11_014826) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "restaurant_id", null: false
+    t.boolean "isSplitVote"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_votes_on_restaurant_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
+  add_foreign_key "votes", "restaurants"
+  add_foreign_key "votes", "users"
 end
